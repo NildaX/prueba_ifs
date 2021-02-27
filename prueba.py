2ecfdf1394bec830ef2104455ffdc7b6813ce17b
NildaX <60120271+NildaX@users.noreply.github.com>
2021-02-27 10:43:56 -0600

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 22:07:49 2021
+
+@author: hp
+"""
+from prueba_ifs import *
+if __name__ == "__main__": 
+    print("tambien corre esto")
+    ifs = prueba_ifs()
+    ifs.imprimir()
+  #  (ifs.window).mainloop()
